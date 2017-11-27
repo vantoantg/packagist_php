7c3e681bdbcb2b92b3a1d4a6ccc6909e280ab77a
Mr.Tona <nguyennguyen.vt88@gmail.com>
2017-11-27 22:39:05 +0700

    aa

---
+++ src/SayHello.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 11/27/2017
+ * Time: 10:33 PM
+ */
+
+namespace tonanguyen\SayHello;
+class SayHello
+{
+	public static function world()
+	{
+		return 'Hello World, Composer!';
+	}
+}
